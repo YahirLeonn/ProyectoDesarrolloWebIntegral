d7a9884aabc789dc87f7048b0836426843593a10
YahirLeonn <yahirleon.antonio@gmail.com>
2025-05-26 09:49:55 -0600

    Ageregando diseno de register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './register.component.html',
   styles: `
     :host {
@@ -12,4 +14,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterComponent { }
+export class RegisterComponent {
+
+  fullName: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+  onRegister() {
+    if (this.password !== this.confirmPassword) {
+      alert('Las contraseñas no coinciden!');
+      return;
+    }
+    // Aquí iría la lógica de registro
+    console.log('Intento de registro con:', this.fullName, this.email, this.password);
+    alert(`Registro Intentado!\nNombre: ${this.fullName}\nEmail: ${this.email}\nPassword: ${this.password}`);
+    // Normalmente, enviarías esto a un servicio de registro
+  }
+ }
